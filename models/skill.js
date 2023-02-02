5363ab7e343dd77c23ef1fdaa64a82c303f4831c
RareTC <trevorjcampbell@aol.com>
2023-02-01 16:10:26 -0800

    Updated Skills to edit and update

--- models/skill.js
+++ models/skill.js
@@ -1,3 +1,5 @@
+// const { update } = require("../controllers/skills");
+
 const skills = [
     {id:123464, skill: 'HTML', fluent: false},
     {id:143423, skill: 'CSS', fluent: false}, 
@@ -6,7 +8,28 @@ const skills = [
 
 module.exports = {
     getAll, 
-    getOne
+    getOne,
+    create,
+    deleteOne,
+    update
+};
+
+function update(id, updatedSkill) {
+    id= parseInt(id);
+    const skill = skills.find(skill => skill.id === id);
+    Object.assign(skill, updatedSkill);
+}
+
+function deleteOne(id) {
+    id = parseInt(id);
+    const idx = skills.findIndex(skill => skill.id === id);
+    skills.splice(idx, 1);
+}
+
+function create(skill) {
+    skill.id = Date.now() % 1000000;
+    skill.fluent = false;
+    skills.push(skill);
 }
 
 function getOne(id) {
--- routes/skills.js
+++ routes/skills.js
@@ -4,8 +4,18 @@ var skillsCtrl = require('../controllers/skills');
 
 /* GET users listing. */
 router.get('/', skillsCtrl.index);
-
+//get todos/new
+router.get('/new', skillsCtrl.new);
 //get skills:id
 router.get('/:id', skillsCtrl.show);
+
+router.get('/:id/edit', skillsCtrl.edit);
+
+router.post('/', skillsCtrl.create);
+
+router.delete('/:id', skillsCtrl.delete);
+
+router.put('/:id', skillsCtrl.update);
+
 
 module.exports = router;
